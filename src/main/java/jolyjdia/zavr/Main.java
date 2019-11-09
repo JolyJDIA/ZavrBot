e36097ec39447672e0bef8a48577c0117b932a95
ZAUR <jolyjdia@gmail.com>
2019-11-09 09:53:42 +0300

    v1.0

--- src/main/java/jolyjdia/zavr/Main.java
+++ src/main/java/jolyjdia/zavr/Main.java
@@ -13,6 +13,7 @@ import java.util.Properties;
 public class Main {
     private static final Properties properties = new Properties();
     private static String accessToken;
+    private static int userId;
 
     static {
         try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
@@ -22,11 +23,12 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        accessToken = properties.getProperty("access_token");
+        userId = Integer.parseInt(properties.getProperty("userId"));
+        accessToken = properties.getProperty("accessToken");
     }
     private static final VkApiClient vkApiClient = new VkApiClient(new HttpTransportClient());
     public static void main(String[] args) throws ClientException, ApiException {
-        UserActor zavr = new UserActor(310289867, accessToken);
+        UserActor zavr = new UserActor(userId, accessToken);
         System.out.println(vkApiClient.friends().get(zavr).execute());
 
     }
